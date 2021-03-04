f5c00bba3d7c5941831fff67715dbc108db8407c
JSmith604 <jacquelinesmith190@gmail.com>
2021-03-04 06:45:00 +0000

    Add exercise 6

--- exercises/exercise_1.rb
+++ exercises/exercise_1.rb
@@ -18,10 +18,3 @@ puts Store.count
 # bundle exec ruby exercises/exercise_1.rb
 
 
-### Exercise 1: Create 3 stores
-
-# 1. Use Active Record's `create` class method multiple times to create 3 stores in the database:
-#   * Burnaby (annual_revenue of 300000, carries men's and women's apparel)
-#   * Richmond (annual_revenue of 1260000 carries women's apparel only)
-#   * Gastown (annual_revenue of 190000 carries men's apparel only)
-# 2. Output (`puts`) the number of the stores using ActiveRecord's `count` method, to ensure that there are three stores in the database.

a4d726e74ec905eb8114eaaef2ecccf15be13dbe
JSmith604 <jacquelinesmith190@gmail.com>
2021-03-04 05:35:19 +0000

    Exercise 1 and 2

--- exercises/exercise_2.rb
+++ exercises/exercise_2.rb
@@ -1,7 +1,26 @@
 require_relative '../setup'
 require_relative './exercise_1'
+require './lib/store.rb'
 
 puts "Exercise 2"
 puts "----------"
 
+# bundle exec ruby exercises/exercise_2.rb
+
 # Your code goes here ...
+
+@store1 = Store.find_by(name: "Burnaby")
+
+@store1.update(name: "Yaletown")
+@store2 = Store.find_by(name: "Richmond")
+
+
+
+
+
+
+
+
+
+
+
